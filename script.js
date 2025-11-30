6153521e13bca5f230685024ccd92a9c5e7f7291
Farid Hussain <faridhussain0011@gmail.com>
2025-11-30 19:41:20 +0530

    add task creation, drag and drop persistence, and localStorage support

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
+let taskData = {};
 const todo = document.querySelector("#todo");
 const progress = document.querySelector("#progress");
 const done = document.querySelector("#done");
+const columns = [todo, progress, done];
 let dragElement = null;
+
+if (localStorage.getItem("tasks")) {
+  const data = JSON.parse(localStorage.getItem("tasks"));
+  console.log(data);
+
+  for (const col in data) {
+    const column = document.querySelector(`#${col}`);
+    data[col].forEach((task) => {
+      const div = document.createElement("div");
+      div.classList.add("task");
+      div.setAttribute("draggable", "true");
+      div.innerHTML = `
+        <h2>${task.title}</h2>
+        <p>${task.desc}</p>
+        <button>Delete</button>
+      `;
+      column.appendChild(div);
+      div.addEventListener("drag", (e) => {
+        dragElement = div;
+      });
+    });
+  }
+}
 
 const tasks = document.querySelectorAll(".task");
 
@@ -31,6 +56,12 @@ function addDragEventsOnColumn(column) {
     console.log("dropped", dragElement, column);
     column.append(dragElement);
     column.classList.remove("hover-over");
+
+    [todo, progress, done].forEach((col) => {
+      const tasks = col.querySelectorAll(".task");
+      const count = col.querySelector(".right");
+      count.innerText = tasks.length;
+    });
   });
 }
 addDragEventsOnColumn(todo);
@@ -51,6 +82,35 @@ modalBg.addEventListener("click", () => {
 });
 
 addTaskButton.addEventListener("click", () => {
-  const taskTitle = document.querySelector("#text-title-input");
-  const taskDesc = document.querySelector("#task-desc-input");
+  const taskTitle = document.querySelector("#text-title-input").value;
+  const taskDesc = document.querySelector("#task-desc-input").value;
+
+  const div = document.createElement("div");
+  div.classList.add("task");
+  div.setAttribute("draggable", "true");
+  div.innerHTML = `
+    <h2>${taskTitle}</h2>
+    <p>${taskDesc}</p>
+    <button>Delete</button>
+  `;
+  todo.appendChild(div);
+
+  columns.forEach((col) => {
+    const tasks = col.querySelectorAll(".task");
+    const count = col.querySelector(".right");
+    taskData[col.id] = Array.from(tasks).map((t) => {
+      return {
+        title: t.querySelector("h2").innerText,
+        desc: t.querySelector("p").innerText,
+      };
+    });
+    localStorage.setItem("tasks", JSON.stringify(taskData));
+    count.innerText = tasks.length;
+  });
+
+  div.addEventListener("drag", (e) => {
+    dragElement = div;
+  });
+
+  modal.classList.remove("active");
 });
